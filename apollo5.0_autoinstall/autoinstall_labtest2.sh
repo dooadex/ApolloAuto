e001a5b28e78fb928db51be76c6fccefae7fb26c
dooadex <72431755+dooadex@users.noreply.github.com>
2020-10-07 15:40:11 +0900

    Create autoinstall_labtest2.sh

---
+++ apollo5.0_autoinstall/autoinstall_labtest2.sh
@@ -0,0 +1,11 @@
+bash ${file_path}/install_set_gcc.sh
+bash ${file_path}/install_nvidia_docker_container_toolkit.sh
+
+#cp ${HOME}/apollo5.0_autoinstall/setup.bash ${HOME}/apollo/cyber/ ## If cyber_node info doesn't work, run this command.
+
+cd ${HOME}/apollo
+bash docker/scripts/dev_start.sh
+bash docker/scripts/dev_into.sh
+
+#bash ./apollo.sh config
+#bash apollo.sh build or bash apollo.sh build_opt_gpu
