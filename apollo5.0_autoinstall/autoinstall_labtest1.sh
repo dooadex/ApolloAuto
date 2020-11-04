e1a5535a109a5a1073f50192c44b48a3b1c8fb0c
ubuntu <dooadex@gmail.com>
2020-11-03 22:32:23 -0800

    delete autoinstall

--- apollo5.0_autoinstall/autoinstall_labtest1.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-file_path=${HOME}/ApolloAuto/apollo5.0_autoinstall
-sudo apt install -y git tar curl
-bash ${file_path}/install_apollo5.0.sh
-bash ${file_path}/install_docker.sh
--- apollo5.0_autoinstall/install_apollo5.0.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-cd ${HOME}
-git clone https://github.com/ApolloAuto/apollo.git
-cd ${HOME}/apollo
-git checkout r5.0.0
-echo "export APOLLO_HOME=$(pwd)" >> ~/.bashrc && source ~/.bashrc
